71bb20a8e5daecc26b06682c3b5d1ef76cb532e9
mrashidnawaz <mrashidnawazpakistan@gmail.com>
2026-03-27 22:02:16 +0500

    Create server.js

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+// Serve static files
+app.use(express.static("public"));
+
+// Home route
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
+});
